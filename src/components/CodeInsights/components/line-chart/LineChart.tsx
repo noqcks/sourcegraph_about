4b7299a056065899660585e512562cac607e82e9
noqcks <benji@093b.org>
2024-04-06 21:59:06 +0000

    Upgrade dependencies

--- src/components/CodeInsights/components/line-chart/LineChart.tsx
+++ src/components/CodeInsights/components/line-chart/LineChart.tsx
@@ -276,7 +276,7 @@ export function LineChartContent<Datum extends object>(props: LineChartContentPr
                                     orientation="left"
                                     tickValues={yTicks}
                                     tickFormat={numberFormatter}
-                                    tickLabelProps={getTickYProps}
+                                    tickLabelProps={(value, index) => getTickYProps(value, index)}
                                     tickComponent={Tick}
                                     axisLineClassName={classNames(styles.axisLine, styles.axisLineVertical)}
                                     tickClassName={classNames(styles.axisTick, styles.axisTickVertical)}
